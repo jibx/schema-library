0d83d7900053e5378c162dc4bbc194215a12f326
Don Corley <don@donandann.com>
2012-09-10 23:39:39 -0700

    Set up list view

--- schema-utilities/examples/android/org.jibx.android.demo.touractivity/src/org/jibx/android/demo/touractivity/MainActivity.java
+++ schema-utilities/examples/android/org.jibx.android.demo.touractivity/src/org/jibx/android/demo/touractivity/MainActivity.java
@@ -1,18 +1,48 @@
 package org.jibx.android.demo.touractivity;
 
+import android.app.ListActivity;
+import android.content.Intent;
 import android.os.Bundle;
-import android.app.Activity;
 import android.view.Menu;
-import android.view.MenuItem;
-import android.support.v4.app.NavUtils;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
 
-public class MainActivity extends Activity {
+public class MainActivity extends ListActivity {
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+ 
+        // storing string resources into Array
+        String[] adobe_products = getResources().getStringArray(R.array.adobe_products);
+ 
+        // Binding resources Array to ListAdapter
+        this.setListAdapter(new ArrayAdapter<String>(this, R.layout.list_item, R.id.label, adobe_products));
+ 
+        ListView lv = getListView();
+ 
+        // listening to single list item on click
+        lv.setOnItemClickListener(new OnItemClickListener() {
+          public void onItemClick(AdapterView<?> parent, View view,
+              int position, long id) {
+ 
+              // selected item
+              String product = ((TextView) view).getText().toString();
+ 
+              // Launching new Activity on selecting single List Item
+              Intent i = new Intent(getApplicationContext(), SingleListItem.class);
+              // sending data to new activity
+              i.putExtra("product", product);
+              startActivity(i);
+ 
+          }
+        });
     }
+//        setContentView(R.layout.activity_main);
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
